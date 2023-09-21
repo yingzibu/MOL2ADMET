ae015a60664a20248c7878543073d8e934e9258e
yingzibu <46125227+yingzibu@users.noreply.github.com>
2023-09-20 20:59:44 -0400

    Update eval_util.py

--- scripts/eval_util.py
+++ scripts/eval_util.py
@@ -1,6 +1,95 @@
 from mycolorpy import colorlist as mcp
 import matplotlib.pyplot as plt
 import sklearn.metrics as metrics
+from rdkit import Chem
+from rdkit.Chem.SaltRemover import SaltRemover
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+MASK = -100
+
+"Reference: https://github.com/Yimeng-Wang/JAK-MTATFP/blob/main/preprocess.py"
+
+from molvs.normalize import Normalizer, Normalization
+from molvs.charge import Reionizer, Uncharger
+import torch.nn as nn
+
+from tdc.single_pred import ADME
+from rdkit import RDLogger
+import warnings
+RDLogger.DisableLog('rdApp.*')
+warnings.filterwarnings("ignore")
+
+from sklearn.metrics import confusion_matrix, roc_auc_score
+from sklearn.metrics import f1_score, accuracy_score, average_precision_score
+
+import math
+
+def get_preds(threshold, probabilities):
+    try:
+        if probabilities.shape[1] == 2: probabilities = probabilities[:, 1]
+    except: pass
+    return [1 if prob > threshold else 0 for prob in probabilities]
+
+def evaluate_model(TP, FP, TN, FN):
+
+    ACCURACY = (TP + TN) / (TP+FP+TN+FN)
+    SE = TP/(TP+FN)
+    recall = SE
+    SP = TN/(TN+FP)
+    weighted_accuracy = (SE + SP) / 2
+
+    precision = TP / (TP + FP)
+    SP = TN/(TN+FP)
+    F1 = 2 * precision * recall /(precision + recall)
+
+    temp = (TP+FP)*(TP+FN)*(TN+FP)*(TN+FN)
+    if temp != 0:
+        MCC = (TP*TN-FP*FN)*1.0/(math.sqrt(temp))
+    else:
+        print('equation for MCC is (TP*TN-FP*FN)*1.0/(math.sqrt(temp))')
+        print('TP, FP, TN, FN', TP, FP, TN, FN)
+        print('temp=0')
+        MCC = 'N/A'
+
+    return ACCURACY,SE, SP, weighted_accuracy, precision, F1, MCC
+
+def evaluate(y_real, y_hat, y_prob):
+    TN, FP, FN, TP = confusion_matrix(y_real, y_hat).ravel()
+    ACCURACY,SE, SP, weighted_accuracy, precision, F1, \
+        MCC  = evaluate_model(TP, FP, TN, FN)
+    try:
+        if y_prob.shape[1] == 2: proba = y_prob[:, 1]
+        else: proba = y_prob
+    except: proba = y_prob
+    AP = average_precision_score(y_real, proba)
+    AUC = roc_auc_score(y_real, proba)
+    print('Accuracy, weighted accuracy, precision, recall/SE, SP,     F1,     AUC,     MCC,     AP')
+    if MCC != 'N/A':
+        print("& %5.3f" % (ACCURACY), " &%7.3f" % (weighted_accuracy), " &%15.3f" % (precision),
+      " &%10.3f" % (SE), " &%5.3f" % (SP), " &%5.3f" % (F1), "&%5.3f" % (AUC),
+      "&%8.3f" % (MCC), "&%8.3f" % (AP))
+    else:
+        print("& %5.3f" % (ACCURACY), " &%7.3f" % (weighted_accuracy), " &%15.3f" % (precision),
+      " &%10.3f" % (SE), " &%5.3f" % (SP), " &%5.3f" % (F1), "&%5.3f" % (AUC), "& ",
+        MCC, "&%8.3f" % (AP))
+
+    return ACCURACY, weighted_accuracy, precision, SE, SP, F1, AUC, MCC, AP
+
+
+def preprocess(smi):
+    mol = Chem.MolFromSmiles(smi)
+    normalizer = Normalizer()
+    new1 = normalizer.normalize(mol)
+    remover = SaltRemover()
+    new2 = remover(new1)
+    neutralize1 = Reionizer()
+    new3 = neutralize1(new2)
+    neutralize2 = Uncharger()
+    new4 = neutralize2(new3)
+    new_smiles = Chem.MolToSmiles(new4, kekuleSmiles=False)
+    return new_smiles
 
 def reg_evaluate(label_clean, preds_clean):
     mae = metrics.mean_absolute_error(label_clean, preds_clean)
@@ -21,3 +110,48 @@ def reg_evaluate(label_clean, preds_clean):
     eval_result_rmse = f'RMSE: {rmse:.3f}'
 
     return eval_result_r2, eval_result_mae, eval_result_rmse
+
+def rename_cols(df, name): return df.rename(columns={'Y':name})
+
+def clean_mol(df:pd.DataFrame):
+    prev_len = len(df)
+    for i in tqdm(range(len(df)), total=len(df), desc='Cleaning mols'):
+        try: df.iloc[i]['Drug'] = preprocess(df.iloc[i]['Drug'])
+        except: df.drop(i)
+    if len(df) != prev_len: print(f'prev len: {prev_len}; after clean: {len(df)}')
+    return df.reset_index(drop=True)
+
+def scal(df): # min max scaling
+    # df_norm = df.loc[:, df.columns!='Drug'].copy()
+    df_norm = df.copy()
+    for col in df_norm.columns:
+        if col == 'Drug': pass
+        else:
+            df_norm[col] = (df_norm[col]-df_norm[col].min()
+                )/(df_norm[col].max()-df_norm[col].min()) * 10
+    # df_norm['Drug'] = df['Drug']
+    return df_norm
+
+
+def get_min(d:dict):
+    min_key = next(iter(d))
+
+    # Iterate over the keys in the dictionary
+    for key in d:
+        # If the value of the current key > the value of max_key, update max_key
+        if d[key] < d[min_key]:
+            min_key = key
+    return min_key, d[min_key]
+
+def plot_loss(train_dict, test_dict, name='test'):
+    fig = plt.figure()
+    plt.plot(list(train_dict.keys()), list(train_dict.values()), label='train')
+    plt.plot(list(test_dict.keys()), list(test_dict.values()), label=name)
+    argmin, min = get_min(test_dict)
+    plt.plot(argmin, min, '*', label=f'min epoch {argmin}')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.title('loss during training')
+    plt.legend()
+    plt.show()
+
