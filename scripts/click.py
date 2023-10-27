7da0f73fc8cfe9e20b6ea2cb1f71e5cd0d283118
yingzibu <46125227+yingzibu@users.noreply.github.com>
2023-10-27 13:04:26 -0400

    Update click.py

--- scripts/click.py
+++ scripts/click.py
@@ -1,8 +1,33 @@
 from pynput.mouse import Button, Controller
 import time
-
+from datetime import datetime
 mouse = Controller()
+time_start = time.time()
+hours = 2
+minutes = 10
 
-while True:
+duration = (hours * 60  + minutes) * 60
+timestamp = time.time()  # Get the current timestamp
+# Convert the timestamp to a datetime object
+dt_object = datetime.fromtimestamp(timestamp)
+
+# Format the datetime object as a string
+formatted_time = dt_object.strftime('%Y-%m-%d %H:%M:%S')
+
+print(f'Schedule clicking for {hours} h {minutes} min')
+print("START time:", formatted_time)
+
+while time.time() - timestamp < duration:
     mouse.click(Button.left, 1)
-    time.sleep(30)
+    time.sleep(100)
+
+print('END CLICKING')
+
+timestamp = time.time()  # Get the current timestamp
+# Convert the timestamp to a datetime object
+dt_object = datetime.fromtimestamp(timestamp)
+
+# Format the datetime object as a string
+formatted_time = dt_object.strftime('%Y-%m-%d %H:%M:%S')
+
+print("Current time:", formatted_time)
